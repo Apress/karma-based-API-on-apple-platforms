421b58ede0dc4565476a836aa4990cb8883c9cf7
StuFF mc <m+git@carrascomolina.com>
2020-05-07 14:46:44 +0200

    The app will now work on iOS as well ;-) and use a Segmented Controller in this case

--- Catalyst/PrivacyWS/PrivacyWS/SceneDelegate.swift
+++ Catalyst/PrivacyWS/PrivacyWS/SceneDelegate.swift
@@ -8,48 +8,70 @@
 
 import UIKit
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate, NSToolbarDelegate {
-	let identifier = NSToolbarItem.Identifier(rawValue: "")
-	var window: UIWindow?
-	
-	enum Chapters: String, CaseIterable {
-		case Nothing
-		case Location
-		case Contacts
-		case Photos
-	}
+enum Chapters: String, CaseIterable {
+	case Nothing
+	case Location
+	case Contacts
+	case Photos
+}
 
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+	var window: UIWindow?
+	var toolBarDelegate: ToolbarDelegate?
+	
 	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-		if let titlebar = (scene as? UIWindowScene)?.titlebar {
+		#if targetEnvironment(macCatalyst)
+		if let titlebar = (scene as? UIWindowScene)?.titlebar, let viewController = window?.rootViewController as? ViewController {
 			let toolbar = NSToolbar()
-			toolbar.centeredItemIdentifier = identifier
-			toolbar.delegate = self
+			toolbar.centeredItemIdentifier = ToolbarDelegate.identifier
+			toolBarDelegate = ToolbarDelegate(viewController: viewController)
+			toolbar.delegate = toolBarDelegate
 			titlebar.toolbar = toolbar
 			titlebar.titleVisibility = .hidden
 		}
+		#endif
+	}
+}
+
+#if targetEnvironment(macCatalyst)
+class ToolbarDelegate: NSObject, NSToolbarDelegate {
+	static let identifier = NSToolbarItem.Identifier(rawValue: "")
+	var viewController: ViewController
+	let selectionChange = SelectionChange()
+	
+	init(viewController: ViewController) {
+		self.viewController = viewController
 	}
 
 	func toolbar(_ toolbar: NSToolbar, itemForItemIdentifier itemIdentifier: NSToolbarItem.Identifier, willBeInsertedIntoToolbar flag: Bool) -> NSToolbarItem? {
-		let group = NSToolbarItemGroup(itemIdentifier: identifier, titles: Chapters.allCases.map { $0.rawValue }, selectionMode: .selectOne, labels: nil, target: self, action: #selector(toolbarGroupSelectionChanged))
+		let group = NSToolbarItemGroup(itemIdentifier: ToolbarDelegate.identifier, titles: Chapters.allCases.map { $0.rawValue }, selectionMode: .selectOne, labels: nil, target: self, action: #selector(toolbarGroupSelectionChanged))
 		group.selectedIndex = 0
 		return group
 	}
 		
 	@objc func toolbarGroupSelectionChanged(sender: NSToolbarItemGroup) {
-		guard let vc = window?.rootViewController as? ViewController else { return }
-		switch Chapters.allCases[sender.selectedIndex] {
-		case .Location: vc.location()
-		case .Contacts: vc.contacts()
-		case .Photos: vc.photos()
-		default: vc.mapView.removeAnnotations(vc.annotations)
-		}
+		selectionChange.selectionChanged(in: viewController, to: sender.selectedIndex)
 	}
 		
 	func toolbarDefaultItemIdentifiers(_ toolbar: NSToolbar) -> [NSToolbarItem.Identifier] {
-		return [identifier]
+		return [Self.identifier]
 	}
 		
 	func toolbarAllowedItemIdentifiers(_ toolbar: NSToolbar) -> [NSToolbarItem.Identifier] {
 		return self.toolbarDefaultItemIdentifiers(toolbar)
+	}
+}
+#else
+class ToolbarDelegate { } // To avoid to mark the declaration of the variable in SceneDelegate
+#endif
+
+class SelectionChange {
+	@objc func selectionChanged(in viewController: ViewController, to index: Int) {
+		switch Chapters.allCases[index] {
+		case .Location: viewController.location()
+		case .Contacts: viewController.contacts()
+		case .Photos: viewController.photos()
+		default: viewController.mapView.removeAnnotations(viewController.mapView.annotations)
+		}
 	}
 }
--- Catalyst/PrivacyWS/PrivacyWS/ViewController.swift
+++ Catalyst/PrivacyWS/PrivacyWS/ViewController.swift
@@ -8,35 +8,80 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class Annotation: NSObject, MKAnnotation {
 	var coordinate = CLLocationCoordinate2D(latitude: 50.79, longitude: 6.48)
 	var image: UIImage?
 }
 
-class ViewController: UIViewController, MKMapViewDelegate {
+//class SegmentedController: UISegmentedControlde
+
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 	@IBOutlet weak var mapView: MKMapView!
+	@IBOutlet weak var segmentedControl: UISegmentedControl!
 	var annotations = [Annotation()]
+	let locationManager = CLLocationManager()
+	let selectionChange = SelectionChange()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		mapView.delegate = self
+		print(mapView.userLocation.coordinate)
+		#if targetEnvironment(macCatalyst)
+		segmentedControl.isHidden = true
+		#else
+		segmentedControl.removeAllSegments()
+		Chapters.allCases.reversed().forEach {
+			segmentedControl.insertSegment(withTitle: $0.rawValue, at: 0, animated: false)
+		}
+		segmentedControl.selectedSegmentIndex = 0
+		#endif
 	}
 
+	@IBAction func didUpdateSegmentedControl() {
+		selectionChange.selectionChanged(in: self, to: segmentedControl.selectedSegmentIndex)
+	}
+	
 	func location() {
-		mapView.addAnnotation(annotations.first!)
+//		mapView.addAnnotation(annotations.first!)
+		// Exercise #1: Show the user's current location
+		locationManager.delegate = self
+		locationManager.requestAlwaysAuthorization()
 	}
 	
 	func contacts() {
+		// Exercise #2: Show the location of the contacts
+		// Exercise #3: When tapping on the face, display the card
 	}
 	
 	func photos() {
+		// Exercice #4: Display photos located on a map
 	}
 	
 	// MARK: MapViewDelegate
-	func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+	func mapView(_ mapView: MKMapView,
+				 viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+		if annotation is MKUserLocation {
+			return nil
+		}
 		let annotationView = MKPinAnnotationView(annotation: annotation,
 												 reuseIdentifier: "\(annotation.coordinate.latitude), \(annotation.coordinate.longitude)")
 		return annotationView
+	}
+	
+	// MARK: LocationManagerDelegate
+	func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+		print(error)
+	}
+	
+	func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+		if status == .authorizedAlways { // We should treat the other cases!
+			locationManager.requestLocation()
+		}
+	}
+	
+	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+		
 	}
 }
